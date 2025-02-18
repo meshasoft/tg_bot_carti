03e3a8aa39550a942e44aeaa207b0a93f814c74e
meshasoft <kostyamishin382@gmail.com>
2025-02-18 22:28:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-TOKEN = '7677111916:AAFt5gZFhHMhUqR_YkgEWFzmBaR_PGp2elM'
-ADMIN_CHAT_ID = 5888156005  # Замените на реальный идентификатор администратора
+TOKEN = 'YOUR_BOT_TOKEN'
+ADMIN_CHAT_ID = 1234123456  # Замените на реальный идентификатор администратора
